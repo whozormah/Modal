58cefca91938b278f3796d2b9948a75bb48784ff
uzoma <uzoma.udoma@gmail.com>
2023-05-27 20:18:27 +0100

    Key Press

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const openCard = document.querySelectorAll(".open");
 const closeCard = document.querySelector(".uil");
 const card = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-console.log(overlay);
 
 for (let i = 0; i < openCard.length; i++) {
   openCard[i].addEventListener("click", function () {
@@ -12,12 +11,34 @@ for (let i = 0; i < openCard.length; i++) {
     overlay.classList.remove("hidden");
   });
 }
-closeCard.addEventListener("click", function () {
-  card.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+// closeCard.addEventListener("click", function () {
+//   card.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
 
-overlay.addEventListener("click", function () {
+// overlay.addEventListener("click", function () {
+//   card.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// DRY for the close card
+// Hence
+
+const closeModal = function () {
   card.classList.add("hidden");
   overlay.classList.add("hidden");
+};
+
+closeCard.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// Listen for a Key Pressed
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape") {
+    if (card.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
 });
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -4,7 +4,6 @@ var openCard = document.querySelectorAll(".open");
 var closeCard = document.querySelector(".uil");
 var card = document.querySelector(".modal");
 var overlay = document.querySelector(".overlay");
-console.log(overlay);
 
 for (var i = 0; i < openCard.length; i++) {
   openCard[i].addEventListener("click", function () {
@@ -12,14 +11,33 @@ for (var i = 0; i < openCard.length; i++) {
     card.classList.remove("hidden");
     overlay.classList.remove("hidden");
   });
-}
+} // closeCard.addEventListener("click", function () {
+//   card.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+// overlay.addEventListener("click", function () {
+//   card.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+// DRY for the close card
+// Hence
 
-closeCard.addEventListener("click", function () {
+
+var closeModal = function closeModal() {
   card.classList.add("hidden");
   overlay.classList.add("hidden");
-});
-overlay.addEventListener("click", function () {
-  card.classList.add("hidden");
-  overlay.classList.add("hidden");
+};
+
+closeCard.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal); // Listen for a Key Pressed
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape") {
+    if (card.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
 });
 //# sourceMappingURL=app.dev.js.map
